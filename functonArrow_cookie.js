5cdaf9bf4f487018360de658d9c28dcb70e8c85a
Nahom Getachew <88863094+nahom-getachew7@users.noreply.github.com>
2022-06-05 07:02:55 +0000

    done

--- functonArrow_cookie.js
+++ functonArrow_cookie.js
@@ -1,4 +1,25 @@
-var x = Math.floor(Math.random()*3);
-document.cookie = "This is Nahi"
-let y= document.cookie
-console.log(y);
+let cookieParser = require('cookie-parser');
+const readline = require("readline");
+game = () => {
+  let max = 2;
+  console.log("What is your username?");
+ const username = "Nahom Getachew";
+  
+ console.log("your username is " + username);
+  while(true){
+    console.log("Enter a number between 1 - "+max+": ")
+    let guess = Math.floor(Math.random() * (max-1))+1;
+    let random = Math.floor(Math.random() * (max-1)) + 1;
+    if(guess == random){
+      max++;
+      console.log("Correct!!");
+    }
+    else{
+      console.log("Wrong Guess, Game over");
+      break
+    }
+  }
+  
+};
+
+game();
