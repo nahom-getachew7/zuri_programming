4808649c7ebaf36f37a58880e0c8b95d2eb49ce8
nahom-getachew7gi5 <ets050012feng@aastu.edu.et>
2022-07-19 10:32:07 +0300

    Email sending node js

---
+++ Email_send/sendEmail.js
@@ -0,0 +1,35 @@
+var nodemailer = require('nodemailer');
+
+//send email
+exports.sendingMail = (email, token) => {
+
+    var email = email;
+    var token = token;
+
+    var mail = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        auth: {
+            user: 'aastulibraryproject@gmail.com', // Your email id
+            pass: 'uzpankczzlcdjhht' // Your password
+        }
+    });
+
+    console.log('email is: ' + email)
+
+    var mailOptions = {
+        from: 'aastulibraryproject@gmail.com',
+        to: email,
+        subject: 'Reset Password Link',
+        html: '<p>You requested for reset password, kindly use this' +
+            '<a href="http://localhost:5000/auth/reset_password?token=' + token + '">' +
+            ' link</a> to reset your password</p>'
+    };
+
+    mail.sendMail(mailOptions, function (error, info) {
+        if (!error)
+            console.log('reset link sent...')
+        else
+            console.log('Error: link not sent' + error)
+    });
+
+}
